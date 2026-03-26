9cc26b585c11c75244e21f7e1b23995347378dba
LEanMist <leandro.chq123@gmail.com>
2026-03-26 10:53:21 -0300

    first commit

---
+++ api_ecomerce/src/test/java/br/com/senai/api_ecomerce/ApiEcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.api_ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
